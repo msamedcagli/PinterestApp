3ca51a66031220dd096e307152992f3c063184f5
Muhammed Samed Çağlı <136382731+msamedcagli@users.noreply.github.com>
2025-04-29 09:13:30 +0300

    Delete main directory

--- main/java/com/msamedcagli/sondeneme/Post.kt
+++
@@ -1,8 +0,0 @@
-package com.msamedcagli.sondeneme
-
-data class Post(
-    val userEmail:String="",
-    val comment: String="",
-    val downloadUrl: String="",
-    val isDownloaded: Boolean=false
-)
